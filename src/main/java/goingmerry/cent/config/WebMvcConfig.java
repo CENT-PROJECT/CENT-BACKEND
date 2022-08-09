02619dfcd0b1c411776deecda4888f9e14b50a23
JIWEON-JEONG <dlektl6@naver.com>
2022-08-09 21:36:47 +0900

    주석 처리

--- src/main/java/goingmerry/cent/config/WebMvcConfig.java
+++ src/main/java/goingmerry/cent/config/WebMvcConfig.java
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        WebMvcConfigurer.super.configureViewResolvers(registry);
-        MustacheViewResolver resolver = new MustacheViewResolver();
-        resolver.setCharset("UTF-8");
-        resolver.setContentType("text/html; charset=UTF-8");
-        resolver.setPrefix("classpath:/templates/");
-        resolver.setSuffix(".html");
-
-        registry.viewResolver(resolver);
-    }
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        WebMvcConfigurer.super.configureViewResolvers(registry);
+//        MustacheViewResolver resolver = new MustacheViewResolver();
+//        resolver.setCharset("UTF-8");
+//        resolver.setContentType("text/html; charset=UTF-8");
+//        resolver.setPrefix("classpath:/templates/");
+//        resolver.setSuffix(".html");
+//
+//        registry.viewResolver(resolver);
+//    }
 
     /**
      * CORS 설정을 추가합니다. CORS 에 대한 자세한 설명은 아래 링크부터 시작하세요.
