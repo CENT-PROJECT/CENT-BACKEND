b44cdfb404910c974d522fe525f6aa579f553331
yoonsr97 <98028565+yoonsr97@users.noreply.github.com>
2022-08-15 19:57:59 +0900

    Update WebMvcConfig.java

--- src/main/java/goingmerry/cent/config/WebMvcConfig.java
+++ src/main/java/goingmerry/cent/config/WebMvcConfig.java
@@ -29,7 +29,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000"
+                        "http://54.180.162.173:3000"
                 );
     }
 }
