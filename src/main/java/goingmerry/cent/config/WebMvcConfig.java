21086602f5adb083ea98e5ef698752957cd05389
yoonsr97 <98028565+yoonsr97@users.noreply.github.com>
2022-08-23 18:16:06 +0900

    Update WebMvcConfig.java

--- src/main/java/goingmerry/cent/config/WebMvcConfig.java
+++ src/main/java/goingmerry/cent/config/WebMvcConfig.java
@@ -28,8 +28,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://54.180.162.173:3000")
-                .allowedOrigins("http://54.180.162.173:80")
+                .allowedOrigins("http://54.180.162.173:3000")               
                 .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
                 
     }
