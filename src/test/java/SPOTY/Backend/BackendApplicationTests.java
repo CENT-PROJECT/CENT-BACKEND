2b6b782b325e7cbb54850d377777908cab13350b
JIWEON-JEONG <dlektl6@naver.com>
2022-10-11 18:14:51 +0900

    [ADD] yml setting

---
+++ src/test/java/SPOTY/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SPOTY.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
