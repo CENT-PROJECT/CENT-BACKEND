68a806f923ff379fa5e4e244b824d2f52466d4e2
정지원 <80501465+JIWEON-JEONG@users.noreply.github.com>
2022-10-15 17:47:20 +0900

    Revert "ERD 기반 USER 도메인 개발"

--- src/main/java/SPOTY/Backend/domain/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package SPOTY.Backend.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- src/test/java/SPOTY/Backend/BackendApplicationTests.java
+++ src/test/java/SPOTY/Backend/BackendApplicationTests.java
@@ -2,7 +2,6 @@ package SPOTY.Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 class BackendApplicationTests {
